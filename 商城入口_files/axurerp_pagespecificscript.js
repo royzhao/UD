47914309a327a43c18831160f0c7876bf7da6548
royzhao <524510356@qq.com>
2013-12-03 18:09:53 +0800

    页面 Signed-off-by: royzhao <524510356@qq.com>

---
+++ 商城入口_files/axurerp_pagespecificscript.js
@@ -0,0 +1,65 @@
+﻿for(var i = 0; i < 64; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u57'] = 'center';gv_vAlignTable['u59'] = 'center';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u44'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u63'] = 'center';gv_vAlignTable['u61'] = 'center';
+u23.style.cursor = 'pointer';
+$axure.eventManager.click('u23', function(e) {
+
+if (true) {
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+});
+gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u46'] = 'center';gv_vAlignTable['u53'] = 'center';
+u18.style.cursor = 'pointer';
+$axure.eventManager.click('u18', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('登录.html');
+
+}
+});
+
+u19.style.cursor = 'pointer';
+$axure.eventManager.click('u19', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('注册.html');
+
+}
+});
+gv_vAlignTable['u55'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u48'] = 'center';
+u22.style.cursor = 'pointer';
+$axure.eventManager.click('u22', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('自由团.html');
+
+}
+});
+
+$axure.eventManager.mouseover('u47', function(e) {
+if (!IsTrueMouseOver('u47',e)) return;
+if (true) {
+
+	SetPanelState('u49', 'pd0u49','none','',500,'none','',500);
+
+}
+});
+
+$axure.eventManager.mouseout('u47', function(e) {
+if (!IsTrueMouseOut('u47',e)) return;
+if (true) {
+
+	SetPanelState('u49', 'pd1u49','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u51'] = 'top';gv_vAlignTable['u33'] = 'center';
---
+++ 自由团_files/axurerp_pagespecificscript.js
@@ -0,0 +1,65 @@
+﻿for(var i = 0; i < 117; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u86'] = 'center';gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u55'] = 'center';gv_vAlignTable['u46'] = 'center';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u77'] = 'center';gv_vAlignTable['u93'] = 'top';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u53'] = 'top';gv_vAlignTable['u87'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u27'] = 'center';
+u7.style.cursor = 'pointer';
+$axure.eventManager.click('u7', function(e) {
+
+if (true) {
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+});
+gv_vAlignTable['u112'] = 'center';gv_vAlignTable['u104'] = 'top';
+u8.style.cursor = 'pointer';
+$axure.eventManager.click('u8', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('商城入口.html');
+
+}
+});
+
+$axure.eventManager.mouseover('u49', function(e) {
+if (!IsTrueMouseOver('u49',e)) return;
+if (true) {
+
+	SetPanelState('u51', 'pd0u51','none','',500,'none','',500);
+
+}
+});
+
+$axure.eventManager.mouseout('u49', function(e) {
+if (!IsTrueMouseOut('u49',e)) return;
+if (true) {
+
+	SetPanelState('u51', 'pd1u51','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u79'] = 'center';gv_vAlignTable['u81'] = 'center';gv_vAlignTable['u97'] = 'top';gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u108'] = 'center';gv_vAlignTable['u71'] = 'center';gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u2'] = 'top';gv_vAlignTable['u92'] = 'center';gv_vAlignTable['u83'] = 'center';gv_vAlignTable['u95'] = 'center';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u43'] = 'center';
+u3.style.cursor = 'pointer';
+$axure.eventManager.click('u3', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('登录.html');
+
+}
+});
+gv_vAlignTable['u90'] = 'center';gv_vAlignTable['u73'] = 'center';gv_vAlignTable['u84'] = 'top';gv_vAlignTable['u20'] = 'center';gv_vAlignTable['u50'] = 'center';gv_vAlignTable['u106'] = 'center';gv_vAlignTable['u99'] = 'center';gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u69'] = 'center';gv_vAlignTable['u114'] = 'top';
+u4.style.cursor = 'pointer';
+$axure.eventManager.click('u4', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('注册.html');
+
+}
+});
+gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u96'] = 'top';gv_vAlignTable['u61'] = 'center';gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u65'] = 'center';gv_vAlignTable['u116'] = 'center';gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u33'] = 'center';gv_vAlignTable['u63'] = 'center';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u48'] = 'center';gv_vAlignTable['u110'] = 'top';gv_vAlignTable['u67'] = 'center';gv_vAlignTable['u88'] = 'top';gv_vAlignTable['u57'] = 'center';gv_vAlignTable['u101'] = 'center';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u14'] = 'center';gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u59'] = 'center';
